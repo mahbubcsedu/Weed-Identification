eeeab8429b65d49bca824d590305739c6c9d43df
Mahbubur Rahman <mahbubcsedu@users.noreply.github.com>
2017-02-19 17:10:49 -0500

    Update db.inc.php

--- weebapp/db.inc.php
+++ weebapp/db.inc.php
@@ -1,8 +1,8 @@
 <?
-$user="crowdsource";
-$password="crowdsource123~";
-$database="weedidapp";
-$host="mpss.csce.uark.edu";
+$user="*****";
+$password="******~";
+$database="*****";
+$host="localhost";
 mysql_connect($host,$user,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 ?>
