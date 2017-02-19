dfa663fbbf17b72087536b3f566145f1ddf3c054
Mahbubur Rahman <mahbubcsedu@users.noreply.github.com>
2017-02-19 16:56:12 -0500

    remove sensitive files

--- weedapp/db.inctest.php
+++
@@ -1,8 +0,0 @@
-<?
-$user="crowdsource";
-$password="crowdsource123~";
-$database="weedidapp";
-$host="mpss.csce.uark.edu";
-mysql_connect($host,$user,$password);
-@mysql_select_db($database) or die( "Unable to select database");
-?>
